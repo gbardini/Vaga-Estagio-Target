7ce747e987d4e6515a3b87aee8fd3a23799c549c
Gabriel Godoy <bardini52@gmail.com>
2024-09-04 21:11:10 -0300

    add: funcao quadradoPerfeito

--- exercicio-2.js
+++ exercicio-2.js
@@ -1,10 +1,20 @@
 // Importação das bibliotecas
 const readline = require("readline");
 
+// Criação da interface para entrada de dados
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+/* Para descobrirmos se um número pertence à sequência de Fibonnaci uma 
+ou ambas dessas expressões 5*n2 + 4 ou 5*n2 – 4 irá retornar um quadrado perfeito */
+
+function quadradoPerfeito(numero) {
+  let raiz = Math.sqrt(numero);
+  let raizExata = Math.floor(raiz);
+  return raizExata === raiz && raiz * raiz === numero;
+}
 
 console.log(
   "Código feito para identificar se o número fornecido pertence à sequência de Fibonnaci"
